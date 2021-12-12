f6a913ad23c47780f119bc5cec33ee8d5e2ef426
Vetri198 <vetri4ever@gmail.com>
2021-12-12 22:47:21 +0530

    maven test commit

---
+++ GithubTest/src/test/java/tests/FirstTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+	@Test 
+	public void firstTestCase() {
+		System.out.println("Git Hub Test");
+		System.out.println("Git Hub Test 1");
+	}
+}
